ea88dbca06378cbb307e3f4c8893ae363a114d59
Andrew Cobby <andrew.cobby@gmail.com>
2013-01-17 15:28:53 +1100

    Bowers power

---
+++ app/scripts/bowers-power.js
@@ -0,0 +1,54 @@
+(function(Backbone, $) {
+  'use strict';
+
+  var BowersPowerView = Backbone.View.extend({
+
+    events: {
+      'change :checkbox': 'toggleBowersPower'
+    },
+
+    initialize: function() {
+      _.bindAll(this);
+      this.$main    = $('#main');
+      this.render();
+    },
+
+    render: function() {
+      this.$el.append('<label><input type=checkbox> Enable Bowers Power</label>');
+    },
+
+    toggleBowersPower: function() {
+      if (this.isChecked()) {
+        this._showBowersPower();
+      } else {
+        this._hideBowersPower();
+      }
+    },
+
+    isChecked: function() {
+      return this.$(':checkbox').is(':checked');
+    },
+
+    _showBowersPower: function() {
+      this.$bubbles = this.$main.find('.bubble');
+
+      _.each(this.$bubbles, function(bubble) {
+        var $bubble = $(bubble);
+        $bubble.data('orig-background-image', $bubble.css('background-image'))
+               .css('background-image', 'url(images/bowers-power.jpg)');
+      });
+    },
+
+    _hideBowersPower: function() {
+      _.each(this.$bubbles, function(bubble) {
+        var $bubble = $(bubble);
+        $bubble.css('background-image', $bubble.data('orig-background-image'));
+      });
+    }
+
+  });
+
+
+  new BowersPowerView({ el: $('#bowers-power')});
+
+}(Backbone, jQuery));
